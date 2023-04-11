f58ffcebd4da51218b00026635213c9f1f865173
nikit <nikit@DESKTOP-5363RTP>
2023-04-11 19:05:20 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include<cstdio>
+#include<iostream>
+class Road {
+public:
+	double Lenght;
+	int Width;
+
+};
+
+int main() {
+	printf("test v0.1 :OK,");
+	int a = 123;
+	Road road;
+	road.Lenght = 300.2;
+	road.Width = 3;
+	std::cout << std::endl << "123455";
+	std::cout << a;
+	std::cout << "Lenght:" << road.Lenght << std::endl;
+	std::cout << "Width:" << road.Width << std::endl;
+
+	return 0;
+}
